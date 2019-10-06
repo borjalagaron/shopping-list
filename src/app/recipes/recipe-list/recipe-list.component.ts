5a94975806c77035e536daa76d8261e0f506007b
Borja Lagaron <borjalagaron@Borjas-MacBook-Pro.local>
2019-10-06 15:00:16 +0200

    Lesson 53 - Displaying Recipe Details - just display (no datab-binding)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Risotto 🍚🍄🧀',
+      'Arroz con queso',
+      'https://img.chefkoch-cdn.de/rezepte/139801060346833/bilder/589190/crop-600x400/champignon-risotto.jpg'
+    ),
+    new Recipe(
+      'Berenjenas rellenas 🍆',
+      'POllas',
+      'https://t1.rg.ltmcdn.com/es/images/4/3/6/img_berenjenas_rellenas_de_carne_picada_y_tomate_62634_600.jpg'
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
